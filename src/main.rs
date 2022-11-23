ba0c79b07420c134b23fb2fc6de2bc46455c9b87
Florentin Schäfer <florentin.schaefer@gmail.com>
2022-11-23 12:54:36 +0100

    Optimized webhook
    both hooks now only use one http handler and aren't  created every time they are used

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,13 @@ use serenity::model::prelude::Ready;
 use serenity::model::webhook::Webhook;
 use serenity::prelude::*;
 
-struct Handler;
+struct Handler {
+    http: Http,
+    wh_one: Option<Webhook>,
+    wh_two: Option<Webhook>,
+    id_one: String,
+    id_two: String,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -20,17 +26,16 @@ impl EventHandler for Handler {
             return;
         }
 
-        let uri = if msg.channel_id.to_string() == env::var("CHANNEL_ONE_ID").expect("Expected CHANNEL_ONE_ID in the environment") {
-            env::var("CHANNEL_TWO_HOOK").expect("Expected CHANNEL_TWO_HOOK in the environment")
-        } else if msg.channel_id.to_string() == env::var("CHANNEL_TWO_ID").expect("Expected CHANNEL_TWO_ID in the environment") {
-            env::var("CHANNEL_ONE_HOOK").expect("Expected CHANNEL_ONE_HOOK in the environment")
+        // Pick correct webhook
+        let webhook = if msg.channel_id.to_string() == self.id_one {
+            &self.wh_two
+        } else if msg.channel_id.to_string() == self.id_two {
+            &self.wh_one
         } else {
             return;
         };
 
-        let http = Http::new("");
-        let webhook = Webhook::from_url(&http, &uri).await.expect("Replace the webhook with your own");
-
+        // Extract attachments
         let mut files = Vec::new();
         let mut filenames = Vec::new();
         for attachment in &msg.attachments {
@@ -38,8 +43,8 @@ impl EventHandler for Handler {
             filenames.push(attachment.filename.to_string());
         }
 
-        webhook
-            .execute(&http, false, |w| {
+        webhook.as_ref().unwrap()
+            .execute(&self.http, false, |w| {
                 w.content(msg.content_safe(&ctx))
                     .username(&msg.author.name)
                     .avatar_url(&msg.author.avatar_url().unwrap_or_default());
@@ -47,14 +52,14 @@ impl EventHandler for Handler {
                 for (file, filename) in zip(files, filenames) {
                     w.add_file(AttachmentType::Bytes {
                         data: Cow::from(file),
-                        filename
+                        filename,
                     });
                 }
 
                 w
             })
             .await
-            .expect("Could not execute webhook.");
+            .expect("Could not execute webhook");
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
@@ -67,8 +72,20 @@ async fn main() {
     dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN in the environment");
 
+    let mut handler = Handler {
+        http: Http::new(""),
+        wh_one: None,
+        wh_two: None,
+        id_one: env::var("CHANNEL_ONE_ID").expect("Expected CHANNEL_ONE_ID in the environment"),
+        id_two: env::var("CHANNEL_TWO_ID").expect("Expected CHANNEL_TWO_ID in the environment"),
+    };
+    handler.wh_one = Some(Webhook::from_url(&handler.http, &env::var("CHANNEL_ONE_HOOK").expect("Expected CHANNEL_ONE_HOOK in the environment"))
+        .await.expect("Creating CHANNEL_ONE_HOOK failed"));
+    handler.wh_two = Some(Webhook::from_url(&handler.http, &env::var("CHANNEL_TWO_HOOK").expect("Expected CHANNEL_TWO_HOOK in the environment"))
+        .await.expect("Creating CHANNEL_TWO_HOOK failed"));
+
     let mut client =
-        Client::builder(&token, GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT).event_handler(Handler).await.expect("Err creating client");
+        Client::builder(&token, GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT).event_handler(handler).await.expect("Err creating client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
